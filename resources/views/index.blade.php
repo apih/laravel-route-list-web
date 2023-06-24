d98c25a52812a19517dbf13d83a41936a20f228e
Mohd Hafizuddin M Marzuki <hafizuddin_83@yahoo.com>
2023-06-24 12:50:15 +0800

    Add dark mode

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -36,6 +36,36 @@
         .bg-orange {
             background-color: var(--bs-orange);
         }
+
+        .form-check-label {
+            cursor: pointer;
+            margin-left: -0.5rem;
+            padding-left: 0.5rem;
+        }
+
+        .theme-toggle {
+            background-color: transparent;
+            border-radius: 50%;
+            border: 2px solid #ffa600;
+            color: #ffa600;
+            line-height: 0;
+            padding: 0.3rem;
+        }
+
+        .scheme-toggle:hover {
+            background-color: #ffa600;
+            color: #fff;
+        }
+
+        [data-bs-theme=dark] .theme-toggle {
+            border-color: #ff82ff;
+            color: #ff82ff;
+        }
+
+        [data-bs-theme=dark] .theme-toggle:hover {
+            background-color: #ff82ff;
+            color: #000;
+        }
     </style>
 </head>
 
@@ -65,7 +95,7 @@
         </div>
 
         <div class="row mb-2">
-            <div class="col-md-10">
+            <div class="col-md-9">
                 @foreach ($columns as $column => $label)
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="checkbox" id="columns.{{ $column }}" x-model="columns.{{ $column }}">
@@ -75,9 +105,18 @@
                     </div>
                 @endforeach
             </div>
-            <div class="col-md-2">
-                <div class="fw-bold float-end">
-                    <span x-show="filteredCount !== routes.length" x-cloak><span x-text="filteredCount"></span> of</span> {{ $routes->count() }} routes
+            <div class="col-md-3">
+                <div class="fw-bold text-end">
+                    <div class="d-inline-block me-1"><span x-show="filteredCount !== routes.length" x-cloak><span x-text="filteredCount"></span> of</span> <span x-text="total">{{ $routes->count() }}</span> routes</div>
+
+                    <button type="button" class="theme-toggle" x-on:click="darkMode = !darkMode">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sun-fill" viewBox="0 0 16 16" x-show="!darkMode" x-cloak>
+                            <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-fill" viewBox="0 0 16 16" x-show="darkMode" x-cloak>
+                            <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/>
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
@@ -139,6 +178,7 @@
                 filteredCount: @json($routes->count()),
                 loaded: false,
                 refreshing: false,
+                darkMode: Alpine.$persist(false).as('rlw_darkMode'),
 
                 search: Alpine.$persist({
                     column: 'all',
@@ -153,6 +193,16 @@
 
                 init: function () {
                     const that = this;
+
+                    function enableDarkMode(value) {
+                        document.documentElement.dataset.bsTheme = value ? 'dark' : 'light';
+                    }
+
+                    enableDarkMode(that.darkMode);
+
+                    that.$watch('darkMode', function (value) {
+                        enableDarkMode(value);
+                    });
 
                     that.$nextTick(function () {
                         that.routes = @json($routes);
