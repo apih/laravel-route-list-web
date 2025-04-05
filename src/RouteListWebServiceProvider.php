5a8fd69f074d7412210ba3479b2ecc3c9c8ad543
Mohd Hafizuddin M Marzuki <hafizuddin_83@yahoo.com>
2025-04-05 21:14:37 +0800

    Update service provider

--- src/RouteListWebServiceProvider.php
+++ src/RouteListWebServiceProvider.php
@@ -7,21 +7,9 @@ use Illuminate\Support\ServiceProvider;
 class RouteListWebServiceProvider extends ServiceProvider
 {
     /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
      * Bootstrap services.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         $this->loadViewsFrom(realpath(__DIR__ . '/../resources/views'), 'routelistweb');
